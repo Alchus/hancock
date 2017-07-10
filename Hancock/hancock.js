2d2bed992ae5ba44feffdac7cdaaf47e48f3baa1
Thomas Sowders <lordalch@gmail.com>
2017-07-10 00:27:02 -0400

    V0.1

--- Hancock/hancock.js
+++ Hancock/hancock.js
@@ -27,6 +27,25 @@ function getEtherchainLatestBlock() {
         });
 }
 
+
+function getEtherchainBlockHash() {
+    var block = parseInt($('#txtTargetBlock').val(), 10);
+    console.log(block);
+    if (+block === +block) {//if block is not NaN. Welcome to Javascript.
+        httpGet('https://etherchain.org/api/block/' + block,
+            function (response) {
+                console.log(response);
+            var blocks = JSON.parse(response).data;
+            if ((blocks.length > 0) && (blocks[0].hash > 0)) {
+                $('#txtBlockHash').val(blocks[0].hash);
+            } else {
+                $('#txtBlockHash').val("Block is not yet available.");
+            }
+        });
+    }
+}
+
+
 function setRandomKey() {
     var data = "";
     var hexAlphabet = "0123456789abcdef"
@@ -38,16 +57,65 @@ function setRandomKey() {
 }
 
 function getWeb3LatestBlock() {
+    if (typeof (web3) == undefined) {
+        setTargetBlock('');
+        $('#web3fail').show();
+        return false;
+    }
     web3.eth.getBlockNumber(function (error, result) {
         if (error) {
             console.log(error);
             setTargetBlock('');
             $('#web3fail').show();
+            return false;
         } else {
             setTargetBlock(result + 5);
             $('#web3fail').hide();
+            return true;
         }
     });
+
+}
+
+function getWeb3BlockHash() {
+    if (typeof (web3) == undefined) {
+        setTargetBlock('');
+        $('#web3fail').show();
+        return false;
+    }
+    web3.eth.getBlock($('#txtTargetBlock').val(), false, function (error, result) {
+        if (error) {
+            console.log(error);
+            setTargetBlock('');
+            $('#web3fail').show();
+            return false;
+        } else {
+            console.log(result);
+            $('#txtBlockHash').val(result.hash);
+            $('#web3fail').hide();
+            return true;
+        }
+    });
+}
+
+function generateRandomNumbers() {
+    $('#txtOutput').val('');
+    //var modulus = parseInt($('#txtModulus').val());
+    var output = '';
+
+    var hash = parseInt($('#txtBlockHash').val());
+    if (isNaN(hash)) return false;
+    //hash = new BigNumber(hash);
+    console.log('block hash: ' + hash);
+
+    var salt = parseInt($('#txtSecretKey').val());
+    //salt = new BigNumber(salt);
+    console.log('key: ' + salt);
+
+    output = web3.sha3(hash + salt);
+    console.log(output);
+    $('#txtOutput').val(output);
+
 }
 
 function copyElementText(element) {
@@ -58,14 +126,35 @@ function copyElementText(element) {
 function generateCommitment() {
     var block = $('#txtTargetBlock').val();
     var key = $('#txtSecretKey').val();
-    var commit = web3.sha3(block + "" + key);
-    $('#txtCommitment').val(commit);
+    if (block && key) {
+        var commit = web3.sha3(block + "" + key);
+        $('#txtCommitment').val(commit);
+    }
+}
+
+function verifyCommitment() {
+    var block = $('#txtTargetBlock').val();
+    var key = $('#txtSecretKey').val();
+    var valid = (web3.sha3(block + "" + key) ==  $('#txtCommitment').val());
+    if (valid) {
+        $('#txtCommitment').addClass("valid").removeClass("invalid");
+    } else {
+        $('#txtCommitment').addClass("invalid").removeClass("valid");
+    }
 }
 
 $(document).ready(function () {
-    console.log("ready!");
     $('.step1').on('input', function () {
-        //console.log($(this).attr('id'));
         generateCommitment();
+        verifyCommitment();
+    });
+
+    $('.step1').on('click', function () {
+        generateCommitment();
+        verifyCommitment();
+    });
+
+    $('.step2').on('input', function () {
+        verifyCommitment();
     });
 });
